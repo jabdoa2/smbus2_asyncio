5e447c5576481e9ea7e119ba9edaa8f135b0c9c9
Gijs Koning <gijs-koning@live.nl>
2020-08-21 12:14:02 +0200

    Refactor methods to new async way. Add write_byte method

--- smbus2_asyncio/__init__.py
+++ smbus2_asyncio/__init__.py
@@ -16,62 +16,65 @@ class SMBus2Asyncio:
             loop = asyncio.get_event_loop()
         self.loop = loop
         self.executor = executor
-        self.lock = asyncio.Lock(loop=loop)
+        self.lock = asyncio.Lock()
 
     def open_sync(self):
         """Open synchronous."""
         self.smbus = SMBus(self.bus)
 
-    @asyncio.coroutine
-    def open(self):
+    async def open(self):
         """Open async."""
         return self.loop.run_in_executor(self.executor, self.open_sync)
 
-    @asyncio.coroutine
-    def read_byte_data(self, i2c_addr, register):
+    async def read_byte_data(self, i2c_addr, register):
         """Read a single byte from a designated register."""
         assert self.smbus
-        yield from self.lock.acquire()
-        try:
-          result = yield from self.loop.run_in_executor(
+        async with self.lock:
+          result = await self.loop.run_in_executor(
             self.executor, self.smbus.read_byte_data, i2c_addr, register)
-        finally:
-          self.lock.release()
+
         return result
 
-    @asyncio.coroutine
-    def read_i2c_block_data(self, i2c_addr, register, length):
+    async def read_i2c_block_data(self, i2c_addr, register, length):
         """Read a block of byte data from a given register."""
         assert self.smbus
-        yield from self.lock.acquire()
-        try:
-          result = yield from self.loop.run_in_executor(
+        async with self.lock:
+          result = await self.loop.run_in_executor(
             self.executor, self.smbus.read_i2c_block_data, i2c_addr, register, length)
-        finally:
-          self.lock.release()
+
         return result
 
-    @asyncio.coroutine
-    def write_byte_data(self, i2c_addr, register, value):
+    async def write_byte(self, i2c_addr, value, force=None):
+        """
+        Write a single byte to a device.
+
+        :param i2c_addr: i2c address
+        :type i2c_addr: int
+        :param value: value to write
+        :type value: int
+        :param force:
+        :type force: Boolean
+        """
+        assert self.smbus
+        async with self.lock:
+            result = await self.loop.run_in_executor(
+                self.executor, self.smbus.write_byte, i2c_addr, value, force)
+
+        return result
+
+    async def write_byte_data(self, i2c_addr, register, value):
         """Write a byte to a given register."""
         assert self.smbus
-        yield from self.lock.acquire()
-        try:
-          result = yield from self.loop.run_in_executor(
+        async with self.lock:
+          result = await self.loop.run_in_executor(
             self.executor, self.smbus.write_byte_data, i2c_addr, register, value)
-        finally:
-          self.lock.release()
-
         return result
 
-    @asyncio.coroutine
-    def write_i2c_block_data(self, i2c_addr, register, data):
+    async def write_i2c_block_data(self, i2c_addr, register, data):
         """Write a block of byte data to a given register."""
         assert self.smbus
-        yield from self.lock.acquire()
-        try:
-          result = yield from self.loop.run_in_executor(
+        async with self.lock:
+          result = await self.loop.run_in_executor(
             self.executor, self.smbus.write_i2c_block_data, i2c_addr, register, data)
-        finally:
-          self.lock.release()
+
         return result
